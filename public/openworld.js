0eb38b8554976cee5caa81af98f06c47e8d7f3aa
Everett Villiger <everettvilliger@Everetts-MacBook-Air.local>
2022-04-13 15:28:28 -0400

    Added assets and attempted pixijs implementation

---
+++ public/openworld.js
@@ -0,0 +1,36 @@
+import * as PIXI from "pixi.js";
+import { CompositeTilemap } from "@pixi/tilemap";
+
+const renderer = PIXI.autoDetectRenderer({
+    width: 800,
+    height: 600
+});
+let stage=  PIXI.Container;
+let tilemap = CompositeTilemap();
+
+document.body.appendChild(renderer.view);
+//Defines loader
+const loader = new PIXI.Loader();
+
+/**
+ * Builds tilemap
+ * -- Adds grass/tough texture on each 32px by 32px square
+ */
+function buildTilemap() {
+    // Clear everything, like a PIXI.Graphics
+    tilemap.clear();
+
+    const resources = loader.resources;
+    const size = 32;
+
+    for (let i = 0; i < 7; i++) {
+        for (let j = 0; j < 5; j++) {
+            tilemap.tile("grass.png", i * size, j * size);
+
+            if (i % 2 === 1 && j % 2 === 1) {
+                tilemap.tile("tough.png", i * size, j * size);
+            }
+        }
+    }
+
+}
